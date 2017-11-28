d0b4f0545875a236e85a17ce47d29054b53f6ab3
HPF <18611640212@163.com>
2017-11-28 17:14:28 +0800

    Demo

--- src/main/java/com/education/Demo.java
+++ src/main/java/com/education/Demo.java
@@ -1,4 +1,15 @@
 package com.education;
 
 public class Demo {
+
+
+    private String a ;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
 }
